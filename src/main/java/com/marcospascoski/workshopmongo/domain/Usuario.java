2cf08105378c2414042d2b1f041dd240e4a816df
Marcos Pascoski <marcospascoski@gmail.com>
2018-11-11 14:59:33 -0300

    Referenciando os posts do usuário

--- src/main/java/com/marcospascoski/workshopmongo/domain/Usuario.java
+++ src/main/java/com/marcospascoski/workshopmongo/domain/Usuario.java
@@ -1,8 +1,11 @@
 package com.marcospascoski.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="usuario")
@@ -13,6 +16,9 @@ public class Usuario implements Serializable{
 	private String id;
 	private String nome;
 	private String email;
+	
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 	
 	public Usuario() {}
 
@@ -47,6 +53,14 @@ public class Usuario implements Serializable{
 		this.email = email;
 	}
 
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -71,5 +85,4 @@ public class Usuario implements Serializable{
 			return false;
 		return true;
 	}
-	
 }
